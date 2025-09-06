1df11b49da832a6f2fed8ce999ae60e1064ef65d
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-05 20:29:30 -0500

    Se desarrollan metodos en laclase metodos

--- Sln_Ejem8/Ejem8/Program.cs
+++ Sln_Ejem8/Ejem8/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*
+  Sumar dos números
+ */
+
+using Ejem8;
+
+Metodos metodos = new ();
+
+Console.WriteLine("Escribe un numero");
+string num1 = Console.ReadLine() ?? "0";
+Console.WriteLine("Escribe otro numero");
+string num2 = Console.ReadLine() ?? "0";
+Console.WriteLine(metodos.Sumar(Convert.ToInt32(num1), Convert.ToInt32(num2)));
